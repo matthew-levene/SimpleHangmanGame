6bb33fc75cd1d74f680511a074090ed88085725b
Matthew Levene <matthew.levene@hotmail.co.uk>
2023-09-10 19:05:42 +0100

    Setup base project files

---
+++ main.py
@@ -0,0 +1,11 @@
+# Hangman Game
+#
+# Basic hangman game where it takes a single letter as input from the user each round
+# Number of rounds is determined by length of word times by 2
+# If the letter is not in the list, a piece of the hangman will be drawn
+#
+# The progress of the hangman will depend on the number of word length
+# e.g. the more characters in the word, the longer it takes to draw.
+
+if __name__ == '__main__':
+    pass
